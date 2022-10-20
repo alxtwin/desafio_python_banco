5116c24bfa2928011d9b13adff2edf830c27a781
alxtwin <alexlhmolina@gmail.com>
2022-10-20 17:55:46 -0300

    senha-checker

--- classes/banco.py
+++ classes/banco.py
@@ -6,16 +6,16 @@ import maskpass
 
 
 class Conta(Cliente):
-    def __init__(self, agencia=0, conta=0, senha='', tipo_conta='', saldo=0):
+    def __init__(self, agencia=0, conta=0, senha='', tipo_conta='', limite_conta=0, saldo=0):
         self.agencia = agencia
         self.conta = conta
         self.senha = senha
         self.tipo_conta = tipo_conta
+        self.limite_conta = limite_conta
         self.saldo = saldo
 
 
 class Banco(Conta):
-
 
     def mensagem_bem_vindo(self):
         print('Bem vindo ao Banco do Python.')
@@ -56,28 +56,44 @@ class Banco(Conta):
         return self.conta
 
     def cadastra_conta(self):
-        tipo_conta = input('Qual tipo de conta deseja criar?:\n ')
+        print('Escolha o tipo de conta:')
+        print('1 - Conta Corrente\n2 - Conta Poupança\n')
+        tipo_conta = input()
         match tipo_conta:
-            case 1:
+            case 'corrente':
                 self.tipo_conta = 'corrente'
                 self.limite_conta = 1000.00
-            case 2:
+            case 'poupança':
                 self.tipo_conta = 'poupança'
                 self.limite_conta = 500.00
-
         self.nome = input('Digite seu nome:\n ')
         self.cpf = input('Digite seu CPF:\n ')
         self.senha = maskpass.askpass(prompt="Crie uma senha:\n ", mask="*")
         self.agencia = self.gera_agencia()
         self.conta = self.gera_conta()
-
         print("---"*10)
         depositar = input('Deseja depositar algum valor? [S/N]\n ')
         if depositar == 'S':
             self.depositar()
 
-
+    # def checa_senha(self):
+    #     senha = maskpass.askpass(prompt="Digite sua senha:\n ", mask="*")
+    #     n = 0
+    #     while n <= 2:
+    #         if senha == self.senha:
+    #             return True
+    #         elif senha != self.senha:
+    #             print('Senha incorreta!')
+    #             n += 1
+    #             print(f'você tem mais {n} tentativas')
+    #             if n == 2:
+    #                 print('Senha incorreta 3 vezes. Tente novamente mais tarde.')
+    #                 break
+    #             else:
+    #                 self.checa_senha()
+        
     def depositar(self):
+        self.checa_senha()
         deposito = float(input('Digite o valor do depósito:\n '))
         print('Depósito realizado com sucesso!')
         self.saldo += deposito
@@ -90,10 +106,11 @@ class Banco(Conta):
             self.menu() 
             
     def sacar(self):
+        self.checa_senha()
         saque = float(input('Digite o valor do saque:\n '))
-        if saque > self.saldo:
-            print('Saldo insuficiente')
-            self.menu()
+        if saque > self.saldo and saque < self.limite_conta:
+            print('saldo insuficiente/limite de saque excedido.')
+            return False
         else:
             self.saldo -= saque
             print('Saque realizado com sucesso!')
@@ -104,15 +121,14 @@ class Banco(Conta):
                 self.sacar()
             elif choice == 'N':
                 self.menu()
-
-
+    
     def mostra_saldo(self):
         print(f'nome do titular: \n{self.nome}'.upper())
         print()
         print(f'cpf do titular: {self.cpf}')
         print()
-        print(f'agência: {self.agencia}')
-        print(f'conta: {self.conta}')
+        print(f'agência: {self.agencia}, conta: {self.conta}')
+        print(f'tipo de conta: {self.tipo_conta.upper()}; limite de saque: R$ {self.limite_conta}')
         print()
         print(f'saldo: \nR$ {self.saldo}')
         if (self.saldo >= 5000):
